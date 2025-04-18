56ed6c917d47e667cc67d1f91349560b6c3ccf6e
Avasyu10 <124695867+Avasyu10@users.noreply.github.com>
2025-04-18 16:22:03 +0530

    Update User.js

--- api/User.js
+++ api/User.js
@@ -145,7 +145,7 @@ const sendVerificationEmail = ({ _id, email }, res) => {
                         from: process.env.AUTH_EMAIL,
                         to: email,
                         subject: "Verify your email",
-                        html: `<p>Verify your email address to complete the signup and login into your account.</p><p>This link <b>expires in 6 hours</b>.</p><p>Press <a href="${currentURL + 'api/verify/' + _id + '/' + uniqueString}">here</a> to proceed.</p>`
+                        html: `<p>Verify your email address to complete the signup and login into your account.</p><p>This link <b>expires in 6 hours</b>.</p><p>Press <a href="${currentURL + '/api/verify/' + _id + '/' + uniqueString}">here</a> to proceed.</p>`
                     };
 
     //hash the uniquestring
